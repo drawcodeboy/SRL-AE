32d22ee66388cd37536deaeebb968dca861fa6a2
drawcodeboy <20rnjsekdns@naver.com>
2024-08-24 21:00:19 +0900

    [Modify] MAELoss: in sample not mean, get sum for big(?) error

--- models/mae_loss.py
+++ models/mae_loss.py
@@ -16,8 +16,9 @@ class MAELoss(nn.Module):
         # (1) 각 차원에 대한 abs
         error = abs(outputs - targets)
         
-        # (2) 다중 차원을 하나의 스칼라 값으로 나타내기 위한 평균 계산
+        # (2) Reconstruction에 대해 original과 큰 격차(error)를 학습하기 위해서 샘플에 대해서 평균이 아니라
+        # sum을 하여 error를 크게 만들도록 하였음. 물론 배치에 대해서는 샘플 에러의 평균 값을 매겨서 학습시킴
         # dim = dim or dims to reduce
-        error = torch.mean(error, dim=(-2, -1))
+        error = torch.sum(error, dim=(-2, -1))
         
         return error
