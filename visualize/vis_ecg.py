8ad3d7ebd6e84f7badcb8215bf6bd73c7b2e6d77
drawcodeboy <20rnjsekdns@naver.com>
2024-10-21 22:39:39 +0900

    [Modify] ECG Visualize for paper

--- visualize/vis_ecg.py
+++ visualize/vis_ecg.py
@@ -5,18 +5,21 @@ from dataloader import load_dataset
 import argparse
 import matplotlib.pyplot as plt
 import numpy as np
-
+import seaborn as sns
+import pandas as pd
 
 def get_args_parser():
     parser = argparse.ArgumentParser(add_help=False)
     
-    parser.add_argument("--index", type=int, default=0)
+    parser.add_argument("--index1", type=int, default=0)
+    parser.add_argument("--index2", type=int, default=1356)
+    parser.add_argument("--savefig", action='store_true')
     
     return parser
 
 def cls_mapping(target):
     cls_dict = {
-        0: "Normal",
+        0: "Normal", # --index=0
         1: "R-on-T Premature Ventricular Contraction",
         2: "Premature Ventricular Contraction", # --index=1356
         3: "Supraventricular Premature beat",
@@ -24,21 +27,62 @@ def cls_mapping(target):
     }
     return cls_dict[target]
 
+def transform_to_df(ds, label=0):
+    df = pd.DataFrame({
+        'timestep': [],
+        'mV': []
+    })
+    
+    for sample, cls in ds:
+        if cls == label:
+            df_temp = pd.DataFrame({
+                'timestep': np.arange(0, len(sample)),
+                'mV': sample
+            })
+            df = pd.concat([df, df_temp])
+            
+    return df
+    
 def main(args):
     ds = load_dataset(preprocessing=False,
                       mode='test')
-    sample, target = ds[args.index]
+    sample_normal, target_normal = ds[args.index1]
+    sample_anomaly, target_anomaly = ds[args.index2]
     
-    x = np.arange(0, len(sample))
-    label = cls_mapping(target)
+    x = np.arange(0, len(sample_normal))
     
-    plt.plot(x, sample, color='r')
-    plt.title(label)
+    label_normal = cls_mapping(target_normal)
+    label_anomaly = cls_mapping(target_anomaly)
     
-    plt.gca().axes.xaxis.set_visible(False)
-
-    plt.show()
-
+    fig, axes = plt.subplots(1, 2, figsize=(8, 4))
+    axes[0].set_xticks([i * 35 for i in range(0, 5)])
+    axes[0].set_title(f"Normal", fontsize=18, pad=10)
+    axes[0].set_ylim(-5, 2)
+    axes[0].set_xlabel('Time step', fontsize=15)
+    axes[0].set_ylabel('mV', fontsize=15)
+    axes[0].grid()
+    
+    axes[1].set_xticks([i * 35 for i in range(0, 5)])
+    axes[1].set_title(f"Anomaly (PVC)", fontsize=18, pad=10)
+    axes[1].set_ylim(-5, 2)
+    axes[1].set_xlabel('Time step', fontsize=15)
+    axes[1].set_ylabel('mV', fontsize=15)
+    axes[1].grid()
+    
+    data_normal = transform_to_df(ds, label=0)
+    data_anomaly = transform_to_df(ds, label=2)
+    
+    sns.lineplot(data=data_normal, x="timestep", y="mV", errorbar='sd', ax=axes[0], color='g') # 
+    sns.lineplot(data=data_anomaly, x="timestep", y="mV", errorbar='sd', ax=axes[1], color='r') # 
+    sns.despine()
+    
+    plt.tight_layout()
+    
+    if args.savefig:
+        plt.savefig(f"figures/ECG_visualization.jpg", dpi=300)
+    else:
+        plt.show()
+    
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("Visualize ECG", parents=[get_args_parser()])
     
