cf1997f89a5fbb18d17bc14a4b8491fae6ad8219
drawcodeboy <20rnjsekdns@naver.com>
2024-10-21 22:44:40 +0900

    [Delete] calls not used models

--- models/__init__.py
+++ models/__init__.py
@@ -9,15 +9,6 @@ def load_model(model_name:str='LSTM-AE',
                                in_out_dim=in_out_dim,
                                hidden_dims=[128, 64, 32, 16, 8])
     
-    elif model_name == 'LSTM-AE-4': # Extra experiments
-        return LSTMAutoencoder(num_layers=4,
-                               in_out_dim=in_out_dim,
-                               hidden_dims=[64, 32, 16, 8])
-        
-    elif model_name == 'LSTM-AE-3': # Extra experiments
-        return LSTMAutoencoder(num_layers=3,
-                               in_out_dim=in_out_dim,
-                               hidden_dims=[32, 16, 8])
         
     elif model_name == 'ResLSTM-AE': # Residual LSTM Experiments
         return LSTMAutoencoder(num_layers=5,
@@ -39,20 +30,6 @@ def load_model(model_name:str='LSTM-AE',
                                hidden_dims=[128, 64, 32, 16, 8],
                                encoder_lstm_type='plain',
                                decoder_lstm_type='res')
-    
-    elif model_name == 'DeResLSTM-AE-4': # Extra experiments
-        return LSTMAutoencoder(num_layers=4,
-                               in_out_dim=in_out_dim,
-                               hidden_dims=[64, 32, 16, 8],
-                               encoder_lstm_type='plain',
-                               decoder_lstm_type='res')
-        
-    elif model_name == 'DeResLSTM-AE-3': # Extra experiments
-        return LSTMAutoencoder(num_layers=3,
-                               in_out_dim=in_out_dim,
-                               hidden_dims=[32, 16, 8],
-                               encoder_lstm_type='plain',
-                               decoder_lstm_type='res')
         
     elif model_name == 'SparLSTM-AE':
         return LSTMAutoencoder(num_layers=5, 
@@ -60,43 +37,11 @@ def load_model(model_name:str='LSTM-AE',
                                hidden_dims=[128, 64, 32, 16, 8],
                                sparsity_param=0.05,
                                sparsity_weight=1e-4)
-    
-    elif model_name == 'SparLSTM-AE-4':
-        return LSTMAutoencoder(num_layers=4, # Extra experiments
-                               in_out_dim=in_out_dim,
-                               hidden_dims=[64, 32, 16, 8],
-                               sparsity_param=0.05,
-                               sparsity_weight=1e-4)
-        
-    elif model_name == 'SparLSTM-AE-3':
-        return LSTMAutoencoder(num_layers=3, # Extra experiments
-                               in_out_dim=in_out_dim,
-                               hidden_dims=[32, 16, 8],
-                               sparsity_param=0.05,
-                               sparsity_weight=1e-4)
         
     elif model_name == 'SparDeResLSTM-AE' or model_name == 'SRL-AE': # Proposed Model
         return LSTMAutoencoder(num_layers=5,
                                in_out_dim=in_out_dim,
                                hidden_dims=[128, 64, 32, 16, 8],
-                               encoder_lstm_type='plain',
-                               decoder_lstm_type='res',
-                               sparsity_param=0.05,
-                               sparsity_weight=1e-4)
-        
-    elif model_name == 'SparDeResLSTM-AE-4':
-        return LSTMAutoencoder(num_layers=4, # Extra experiments
-                               in_out_dim=in_out_dim,
-                               hidden_dims=[64, 32, 16, 8],
-                               encoder_lstm_type='plain',
-                               decoder_lstm_type='res',
-                               sparsity_param=0.05,
-                               sparsity_weight=1e-4)
-    
-    elif model_name == 'SparDeResLSTM-AE-3':
-        return LSTMAutoencoder(num_layers=3, # Extra experiments
-                               in_out_dim=in_out_dim,
-                               hidden_dims=[32, 16, 8],
                                encoder_lstm_type='plain',
                                decoder_lstm_type='res',
                                sparsity_param=0.05,
